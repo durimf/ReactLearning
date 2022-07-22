52fc621d8cf2bb517418cf621971db5a3c0922d6
durimf <durimfetahaj@gmail.com>
2022-07-22 10:11:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-lrqyvp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-lrqyvp)
---
+++ index.js
@@ -0,0 +1,12 @@
+function MainContent() {
+  return <h1>I am learning React!</h1>;
+}
+
+ReactDOM.render(
+  <div>
+    <MainContent />
+  </div>,
+  document.getElementById('root')
+);
+
+
